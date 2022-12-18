6674ddc050aab4a0d299d3573b058a46f847bb34
its julii <nanji828@gmail.com>
2022-12-18 12:00:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Moon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double srednaSkorost = double.Parse(Console.ReadLine());
+            double litriGorivoZa100km = double.Parse(Console.ReadLine());
+            int raztoqnie = 384400;
+
+            double otivaneIvrushtane = raztoqnie * 2;
+            double time = Math.Ceiling(otivaneIvrushtane / srednaSkorost);
+            double totalTime = time + 3;
+            double gorivo = (litriGorivoZa100km * otivaneIvrushtane) / 100;
+
+            Console.WriteLine(totalTime);
+            Console.WriteLine(gorivo);
+
+
+
+        }
+    }
+}
